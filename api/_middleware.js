79a8a68744a4887842d2b3e8fc8fe25c596b5c0c
mjo-at-github <mjo>
2025-06-13 12:10:01 +0200

    _middleware amended

--- api/_middleware.js
+++ api/_middleware.js
@@ -1,5 +1,6 @@
 export default function middleware(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://mjo-at-github.github.io');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   return res;
 }
